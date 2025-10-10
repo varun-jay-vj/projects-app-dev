82a53e3ad0ed610b4f117d497595532516d86c89
Value-Maximized <varun.jay@icloud.com>
2025-10-10 05:44:22 +0530

    Create variable_types.py

---
+++ basics/data structures/variable_types.py
@@ -0,0 +1,8 @@
+ #### TUPLES ###
+# Similar to a list, but items cannot be modified
+def TupleDataStructures():
+         print("Tuples")
+         dimensions = (1920, 1080)
+         resolutions = ('720p', '1080p', '4K')
+         print(dimensions)
+         print(resolutions)
